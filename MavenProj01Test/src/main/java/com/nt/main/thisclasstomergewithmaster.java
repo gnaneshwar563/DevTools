c7a23cf9ee476b9bba3d57f94790e82ef04ebc17
gnaneshwar563 <gnaneshwar563@gmail.com>
2026-02-17 19:28:04 -0600

    yui

--- MavenProj01Test/src/main/java/com/nt/main/thisclasstomergewithmaster.java
+++ MavenProj01Test/src/main/java/com/nt/main/thisclasstomergewithmaster.java
@@ -4,5 +4,6 @@ public class thisclasstomergewithmaster {
 public void method1() {
 	System.out.println("Hello");
 	System.out.println("thisclasstomergewithmaster.method1()");
+	System.out.println("thisclasstomergewithmaster.method2()");
 }
 }
