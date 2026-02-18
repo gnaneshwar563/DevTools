38b3ead7b0477f548d604cb3bd5e800c5adf1a86
gnaneshwar563 <gnaneshwar563@gmail.com>
2026-02-17 18:40:48 -0600

    flict

--- MavenProj01Test/src/main/java/com/nt/main/A1.java
+++ MavenProj01Test/src/main/java/com/nt/main/A1.java
@@ -4,7 +4,7 @@ public class A1 {
 public void hello() {
 	System.out.println("hello world channge from masters");
 
-	System.out.println("hello world channge from msater3");
+	System.out.println("hello world channge from msater3");//merge
 
 
 
