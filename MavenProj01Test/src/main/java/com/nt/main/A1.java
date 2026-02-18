9ef3342938551418b157a288c3a529f6aa429aea
gnaneshwar563 <gnaneshwar563@gmail.com>
2026-02-17 18:17:46 -0600

    cg

--- MavenProj01Test/src/main/java/com/nt/main/A1.java
+++ MavenProj01Test/src/main/java/com/nt/main/A1.java
@@ -3,6 +3,7 @@ package com.nt.main;
 public class A1 {
 public void hello() {
 	System.out.println("hello world channge from masters");
+	System.out.println("hello world channge from msater1");
 
 }
 }
