d1af947c3c6c5b47a3dc36d361263ebea3701b18
gnaneshwar563 <gnaneshwar563@gmail.com>
2026-02-18 17:58:52 -0600

    hjg

--- MavenProj01Test/src/main/java/com/nt/main/A1.java
+++ MavenProj01Test/src/main/java/com/nt/main/A1.java
@@ -5,6 +5,7 @@ public void hello() {
 	System.out.println("hello world channge from masters ");
 System.out.println("writen from masters ");
 System.out.println("written from dev1 in middle of his new feature ");
+System.out.println("written from master in middle of his new feature ");
 
 
 
