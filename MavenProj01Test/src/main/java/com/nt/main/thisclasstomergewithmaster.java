3e6fa8689a50996d1e610487f4f9f6998076941c
gnaneshwar563 <gnaneshwar563@gmail.com>
2026-02-17 19:57:31 -0600

    Update thisclasstomergewithmaster.java

--- MavenProj01Test/src/main/java/com/nt/main/thisclasstomergewithmaster.java
+++ MavenProj01Test/src/main/java/com/nt/main/thisclasstomergewithmaster.java
@@ -4,6 +4,6 @@ public class thisclasstomergewithmaster {
 public void method1() {
 	System.out.println("Hello");
 	System.out.println("thisclasstomergewithmaster.method1()");
-	System.out.println("thisclasstomergewithmaster.method2()");
+	System.out.println("thisclasstomergewithmaster.method22()");
 }
 }
