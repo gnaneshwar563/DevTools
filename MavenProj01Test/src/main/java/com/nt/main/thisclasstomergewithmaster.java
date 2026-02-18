c64ce7d4e3c2c211f6f2b7d52800d0a26360da06
gnaneshwar563 <gnaneshwar563@gmail.com>
2026-02-17 19:26:25 -0600

    kul

--- MavenProj01Test/src/main/java/com/nt/main/thisclasstomergewithmaster.java
+++ MavenProj01Test/src/main/java/com/nt/main/thisclasstomergewithmaster.java
@@ -4,5 +4,7 @@ public class thisclasstomergewithmaster {
 public void method1() {
 	System.out.println("Hello");
 	System.out.println("thisclasstomergewithmaster.method1()");
+	System.out.println("thisclasstomergewithmaster.method2()");
+	
 }
 }
