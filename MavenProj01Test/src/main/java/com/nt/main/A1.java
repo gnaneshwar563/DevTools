dd7dd85c192c2bca2de2c8284a0287f2252dc6b7
gnaneshwar563 <gnaneshwar563@gmail.com>
2026-02-18 18:12:57 -0600

    sefdvx

--- MavenProj01Test/src/main/java/com/nt/main/A1.java
+++ MavenProj01Test/src/main/java/com/nt/main/A1.java
@@ -1,14 +1,17 @@
 package com.nt.main;
 
 public class A1 {
+
 public void hello() {
 	System.out.println("hello world channge from masters ");
 System.out.println("writen from masters ");
 System.out.println("written from dev1 in middle of his new feature ");
 System.out.println("written from master in middle of his new feature ");
+	}
+	public void framStash() {
+		System.out.println("hello world channge from masters ");
+		System.out.println("A1.hello()");
+		System.out.println("Hello this message from dev1");
 
-
-
-
-}
+	}
 }
